2eebf565b6ac7f28c4ed8d2aa8847c07c55dcb0c
권형기 <flex@gwonhyeong-giui-MacBookPro.local>
2025-04-22 19:20:09 +0900

    chore: add debug header for CORS test

--- api/components.js
+++ api/components.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("X-Debug-Route", "components-handler");
 
   // ✅ OPTIONS 요청에 대한 빠른 응답
   if (req.method === "OPTIONS") {
