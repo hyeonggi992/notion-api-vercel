496d1e83d3dbbbf8c428612f258a0760e5bdfca3
권형기 <flex@gwonhyeong-giui-MacBookPro.local>
2025-04-22 17:50:56 +0900

    feat: add API route

--- api/components.js
+++ api/components.js
@@ -1,4 +1,5 @@
-const { Client } = require('@notionhq/client');
+require("dotenv").config();
+const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const databaseId = process.env.NOTION_DB_ID;
@@ -7,17 +8,17 @@ module.exports = async (req, res) => {
   try {
     const response = await notion.databases.query({ database_id: databaseId });
 
-    const results = response.results.map(page => {
+    const results = response.results.map((page) => {
       const props = page.properties;
       return {
-        ComponentName: props.ComponentName?.title?.[0]?.plain_text || '',
-        Description: props.Description?.rich_text?.[0]?.plain_text || ''
+        ComponentName: props.ComponentName?.title?.[0]?.plain_text || "",
+        Description: props.Description?.rich_text?.[0]?.plain_text || "",
       };
     });
 
     res.status(200).json(results);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Failed to fetch data from Notion');
+    res.status(500).send("Failed to fetch data from Notion");
   }
 };
